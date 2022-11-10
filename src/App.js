b669c252006c9dbe1f4b3a1bfa629d9a65be11fa
Waldemar Głaz <w.glaz@cheil.com>
2022-11-10 18:07:09 +0100

    fetch data from api

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./components/Header";
 import InputForm from "./components/InputForm";
 import Filter from "./components/Filter";
 import "./App.css";
 function App() {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  useEffect(() => {
+    fetch("https://restcountries.com/v3.1/all")
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error(`This is an HTTP error: The status is ${res.status}`);
+        }
+        return res.json();
+      })
+      .then((actualData) => {
+        setData(actualData);
+        setError(null);
+        console.log(data);
+      })
+      .catch((err) => {
+        setError(err.message);
+        setData(null);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
   return (
     <div className="App">
       <Header />
