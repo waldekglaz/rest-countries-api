a7af39a30feed5ddedf3625cd9cf480777513bf7
Waldemar Głaz <w.glaz@cheil.com>
2022-11-11 14:38:51 +0100

    nativeName getting data issue fixed

--- src/components/DetailPage.js
+++ src/components/DetailPage.js
@@ -1,15 +1,20 @@
 import React from "react";
+import styles from "./DetailPage.module.css";
+import { BsArrowLeft } from "react-icons/bs";
 
 function DetailPage({ detailData, data, onClick }) {
   console.log(detailData);
   const curr = Object.values(detailData.currencies);
   const lang = Object.values(detailData.languages).join(", ");
+  const nativeNames = Object.values(detailData.name.nativeName);
   const borders = detailData.borders;
   const countryCode = detailData.cca3;
-  console.log(countryCode, "country Code");
+  // const nativeName = detailData.name.nativeName.ara.official;
+  const nativeNameParent = detailData.name.nativeName;
+  console.log(nativeNames[0].official, "native name");
   const borderCountries = [];
   for (const el in borders) {
-    console.log(borders[el]);
+    // console.log(borders[el]);
 
     for (const el2 in data) {
       if (data[el2].cca3 === borders[el]) {
@@ -17,18 +22,19 @@ function DetailPage({ detailData, data, onClick }) {
       }
     }
   }
-  console.log(borderCountries);
+  // console.log(borderCountries);
 
   return (
-    <div className="detail-page">
+    <div className={styles["detail-page"]}>
       <button type="button" onClick={onClick}>
+        <BsArrowLeft />
         Back
       </button>
       <img src={detailData.flags.png} />
       <h2>{detailData.name.common}</h2>
       <ul>
         <li>
-          Native Name: <span>{detailData.altSpellings.at(-1)}</span>
+          Native Name: <span>{nativeNames[0].official}</span>
         </li>
         <li>
           Population: <span>{detailData.population}</span>
@@ -39,7 +45,7 @@ function DetailPage({ detailData, data, onClick }) {
         <li>
           Sub Region: <span>{detailData.subregion}</span>
         </li>
-        <li>
+        <li style={{ marginBottom: "42px" }}>
           Capital: <span>{detailData.capital}</span>
         </li>
         <li>
