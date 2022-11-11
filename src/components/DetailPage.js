95f85181a910d02b96e2d17d76f4997a6492fefc
Waldemar Głaz <w.glaz@cheil.com>
2022-11-11 15:52:18 +0100

    DetailPage min-width:1440px styled

--- src/components/DetailPage.js
+++ src/components/DetailPage.js
@@ -10,11 +10,11 @@ function DetailPage({ detailData, data, onClick }) {
   const borders = detailData.borders;
   const countryCode = detailData.cca3;
 
-  // creating array of neighbour countries full names
+  // creating array of neighbour countries full names based on coutry code
   const borderCountries = [];
   for (const el in borders) {
     for (const el2 in data) {
-      if (countryCode === borders[el]) {
+      if (data[el2].cca3 === borders[el]) {
         borderCountries.push(data[el2].name.common);
       }
     }
@@ -26,48 +26,57 @@ function DetailPage({ detailData, data, onClick }) {
         <BsArrowLeft />
         Back
       </button>
-      <img src={detailData.flags.png} />
-      <h2>{detailData.name.common}</h2>
-      <ul>
-        <li>
-          Native Name: <span>{nativeNames[0].official}</span>
-        </li>
-        <li>
-          Population: <span>{detailData.population}</span>
-        </li>
-        <li>
-          Region: <span>{detailData.region}</span>
-        </li>
-        <li>
-          Sub Region: <span>{detailData.subregion}</span>
-        </li>
-        <li style={{ marginBottom: "42px" }}>
-          Capital: <span>{detailData.capital}</span>
-        </li>
-        <li>
-          Top Level Domain: <span>{detailData.tld}</span>
-        </li>
-        <li>
-          Currencies: <span>{curr[0].name}</span>
-        </li>
-        <li>
-          Languages: <span>{lang}</span>
-        </li>
-      </ul>
-      <div className={styles["borders-wrapper"]}>
-        <p>Border Countries:</p>
-        <div className={styles.borders}>
-          {borderCountries.length === 0 && (
-            <span className={styles.border}>None</span>
-          )}
-          {borderCountries.length > 0 &&
-            borderCountries.map((country) => {
-              return (
-                <span className={styles.border} key={country}>
-                  {country}
-                </span>
-              );
-            })}
+      <div className={styles["country-wrapper"]}>
+        <img src={detailData.flags.png} />
+        <div className={["text-content"]}>
+          <h2>{detailData.name.common}</h2>
+          <div className={styles["list-container"]}>
+            <ul>
+              <li>
+                Native Name: <span>{nativeNames[0].official}</span>
+              </li>
+              <li>
+                Population: <span>{detailData.population}</span>
+              </li>
+              <li>
+                Region: <span>{detailData.region}</span>
+              </li>
+              <li>
+                Sub Region: <span>{detailData.subregion}</span>
+              </li>
+              <li>
+                Capital: <span>{detailData.capital}</span>
+              </li>
+            </ul>
+
+            <ul>
+              <li>
+                Top Level Domain: <span>{detailData.tld}</span>
+              </li>
+              <li>
+                Currencies: <span>{curr[0].name}</span>
+              </li>
+              <li>
+                Languages: <span>{lang}</span>
+              </li>
+            </ul>
+          </div>
+          <div className={styles["borders-wrapper"]}>
+            <p>Border Countries:</p>
+            <div className={styles.borders}>
+              {borderCountries.length === 0 && (
+                <span className={styles.border}>None</span>
+              )}
+              {borderCountries.length > 0 &&
+                borderCountries.map((country) => {
+                  return (
+                    <span className={styles.border} key={country}>
+                      {country}
+                    </span>
+                  );
+                })}
+            </div>
+          </div>
         </div>
       </div>
     </div>
